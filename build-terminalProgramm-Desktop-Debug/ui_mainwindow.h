05697c503630d5aecbf7aff5950507970d3fc12f
Ebobo-2020 <dno.teaam@gmail.com>
2020-01-13 14:34:50 +0700

    test5

--- build-terminalProgramm-Desktop-Debug/ui_mainwindow.h
+++ build-terminalProgramm-Desktop-Debug/ui_mainwindow.h
@@ -14,8 +14,11 @@
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QButtonGroup>
 #include <QtWidgets/QHeaderView>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QListWidget>
 #include <QtWidgets/QMainWindow>
 #include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
 #include <QtWidgets/QStatusBar>
 #include <QtWidgets/QToolBar>
 #include <QtWidgets/QWidget>
@@ -25,25 +28,64 @@ QT_BEGIN_NAMESPACE
 class Ui_MainWindow
 {
 public:
+    QWidget *centralWidget;
+    QLabel *label_4;
+    QLabel *label_5;
+    QLabel *label_6;
+    QListWidget *listWidget;
+    QPushButton *pushButton;
+    QPushButton *pushButton_2;
+    QPushButton *pushButton_3;
     QMenuBar *menuBar;
     QToolBar *mainToolBar;
-    QWidget *centralWidget;
     QStatusBar *statusBar;
 
     void setupUi(QMainWindow *MainWindow)
     {
         if (MainWindow->objectName().isEmpty())
             MainWindow->setObjectName(QStringLiteral("MainWindow"));
-        MainWindow->resize(400, 300);
+        MainWindow->resize(594, 374);
+        centralWidget = new QWidget(MainWindow);
+        centralWidget->setObjectName(QStringLiteral("centralWidget"));
+        label_4 = new QLabel(centralWidget);
+        label_4->setObjectName(QStringLiteral("label_4"));
+        label_4->setGeometry(QRect(10, 240, 61, 71));
+        QFont font;
+        font.setPointSize(39);
+        label_4->setFont(font);
+        label_5 = new QLabel(centralWidget);
+        label_5->setObjectName(QStringLiteral("label_5"));
+        label_5->setGeometry(QRect(20, 10, 41, 61));
+        label_5->setFont(font);
+        label_6 = new QLabel(centralWidget);
+        label_6->setObjectName(QStringLiteral("label_6"));
+        label_6->setGeometry(QRect(20, 120, 41, 61));
+        label_6->setFont(font);
+        listWidget = new QListWidget(centralWidget);
+        listWidget->setObjectName(QStringLiteral("listWidget"));
+        listWidget->setGeometry(QRect(80, 0, 461, 341));
+        pushButton = new QPushButton(centralWidget);
+        pushButton->setObjectName(QStringLiteral("pushButton"));
+        pushButton->setGeometry(QRect(540, 0, 41, 81));
+        pushButton->setFont(font);
+        pushButton_2 = new QPushButton(centralWidget);
+        pushButton_2->setObjectName(QStringLiteral("pushButton_2"));
+        pushButton_2->setGeometry(QRect(540, 120, 41, 81));
+        pushButton_2->setFont(font);
+        pushButton_3 = new QPushButton(centralWidget);
+        pushButton_3->setObjectName(QStringLiteral("pushButton_3"));
+        pushButton_3->setGeometry(QRect(540, 230, 41, 81));
+        QFont font1;
+        font1.setPointSize(20);
+        pushButton_3->setFont(font1);
+        MainWindow->setCentralWidget(centralWidget);
         menuBar = new QMenuBar(MainWindow);
         menuBar->setObjectName(QStringLiteral("menuBar"));
+        menuBar->setGeometry(QRect(0, 0, 594, 22));
         MainWindow->setMenuBar(menuBar);
         mainToolBar = new QToolBar(MainWindow);
         mainToolBar->setObjectName(QStringLiteral("mainToolBar"));
-        MainWindow->addToolBar(mainToolBar);
-        centralWidget = new QWidget(MainWindow);
-        centralWidget->setObjectName(QStringLiteral("centralWidget"));
-        MainWindow->setCentralWidget(centralWidget);
+        MainWindow->addToolBar(Qt::TopToolBarArea, mainToolBar);
         statusBar = new QStatusBar(MainWindow);
         statusBar->setObjectName(QStringLiteral("statusBar"));
         MainWindow->setStatusBar(statusBar);
@@ -56,6 +98,12 @@ public:
     void retranslateUi(QMainWindow *MainWindow)
     {
         MainWindow->setWindowTitle(QApplication::translate("MainWindow", "MainWindow", Q_NULLPTR));
+        label_4->setText(QApplication::translate("MainWindow", "\342\244\266", Q_NULLPTR));
+        label_5->setText(QApplication::translate("MainWindow", "\342\206\222", Q_NULLPTR));
+        label_6->setText(QApplication::translate("MainWindow", "\342\206\220", Q_NULLPTR));
+        pushButton->setText(QApplication::translate("MainWindow", "\342\206\221", Q_NULLPTR));
+        pushButton_2->setText(QApplication::translate("MainWindow", "\342\206\223", Q_NULLPTR));
+        pushButton_3->setText(QApplication::translate("MainWindow", "OK", Q_NULLPTR));
     } // retranslateUi
 
 };
--- terminalProgramm/mainwindow.cpp
+++ terminalProgramm/mainwindow.cpp
@@ -1,14 +1,60 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "QMessageBox"
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ui->listWidget->addItem("Menu1");
+     ui->listWidget->addItem("Menu2");
+      ui->listWidget->addItem("Menu3");
+       ui->listWidget->addItem("Menu4");
+        ui->listWidget->addItem("Menu5");
+         ui->listWidget->addItem("Menu16");
+          ui->listWidget->addItem("Menu7");
+           ui->listWidget->addItem("Menu18");
+            ui->listWidget->addItem("lox");
+             ui->listWidget->addItem("lox");
+              ui->listWidget->addItem("lox");
+               ui->listWidget->addItem("lox");
+                ui->listWidget->addItem("lox");
+                 ui->listWidget->addItem("lox");
+                  ui->listWidget->addItem("lox");
+                   ui->listWidget->addItem("lox");
+                   ui->listWidget->addItem("Menu1");
+                    ui->listWidget->addItem("Menu2");
+                     ui->listWidget->addItem("Menu3");
+                      ui->listWidget->addItem("Menu4");
+                       ui->listWidget->addItem("Menu5");
+                        ui->listWidget->addItem("Menu16");
+                         ui->listWidget->addItem("Menu7");
+                          ui->listWidget->addItem("Menu18");
+                           ui->listWidget->addItem("lox");
+                            ui->listWidget->addItem("lox");
+                             ui->listWidget->addItem("lox");
+                              ui->listWidget->addItem("lox");
+                               ui->listWidget->addItem("lox");
+                                ui->listWidget->addItem("lox");
+                                 ui->listWidget->addItem("lox");
+                                  ui->listWidget->addItem("lox");
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+
+    ui->listWidget->item(3)->setSelected(true);
+
+}
+
+void MainWindow::on_pushButton_3_clicked()
+{
+    ui->statusBar->showMessage(ui->listWidget->currentItem()->text());  ;
 }
